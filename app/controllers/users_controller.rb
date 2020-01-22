dcf9f3a34fc2877a678693d83a4602dd158d3bb2
hfurumiya <bosnyctokosa@gmail.com>
2020-01-22 06:36:12 +0000

    ユーザー登録ページ完了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,25 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
   end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = 'やったね！成功!!'
+      redirect_to @user
+    else
+      flash.now[:warning] = 'あかん、下記のエラー見て直して'
+      render :new
+    end
+  end
+  
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
