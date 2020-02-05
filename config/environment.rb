25c79da162220855c9509f8d3d82d4c96bce10e3
hfurumiya <bosnyctokosa@gmail.com>
2020-02-05 06:32:00 +0000

    Fnished jpn-format

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# pagenate設定した後のラベルの日本語化
+WillPaginate::ViewHelpers.pagination_options[:previous_label] = '前ページ'
+WillPaginate::ViewHelpers.pagination_options[:next_label] = '次ページ'
