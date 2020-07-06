c55a0b00e7fb0d6e921226ba44c5afb3bae0453e
hfurumiya <bosnyctokosa@gmail.com>
2020-07-06 03:31:49 +0000

    did it again

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     
     if user && user.authenticate(params[:session][:password])
-      flash[:success] = 'ログインできました'
       log_in user
+      flash[:success] = 'ログインできました'
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_back_or user
     else
