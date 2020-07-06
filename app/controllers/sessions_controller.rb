2b9490e14a65f38a5b3894c095ab80b8197f462f
hfurumiya <bosnyctokosa@gmail.com>
2020-07-06 03:58:01 +0000

    did it again

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,6 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by(email: params[:session][:email].downcase)
-    
     if user && user.authenticate(params[:session][:password])
       log_in user
       flash[:success] = 'ログインできました'
