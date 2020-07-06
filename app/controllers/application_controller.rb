fba9d769b802373ebc7c23f6f5029aca2981ba3b
hfurumiya <bosnyctokosa@gmail.com>
2020-07-06 03:05:46 +0000

    Edited again

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,6 @@ class ApplicationController < ActionController::Base
 
     
     def correct_user
-      # @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
     end
     
