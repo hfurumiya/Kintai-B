a44088d7d8b3402c562c927c547235d2f1672d80
hfurumiya <bosnyctokosa@gmail.com>
2020-07-15 10:47:36 +0000

    updated

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,23 @@
 class SessionsController < ApplicationController
   def new
+    
   end
   
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      flash[:success] = 'ログインできました'
       params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_back_or user
     else
-      flash.now[:warning] = '登録できませんでした'
+      flash.now[:danger] = "ログインできませんでした"
       render :new
     end
   end
   
   def destroy
     log_out if logged_in?
-    flash[:success] = 'ログアウトしました。'
+    flash[:success] = "ログアウトしました"
     redirect_to root_url
   end
   
